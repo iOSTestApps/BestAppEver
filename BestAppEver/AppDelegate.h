3983db47e872cffa89ac3c10786468ba78c904cc
Christopher Stott <christopherstott@gmail.com>
2015-05-06 10:37:26 -0700

    Initial Commit

---
+++ BestAppEver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BestAppEver
+//
+//  Created by Chris on 2015-05-06.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BestAppEver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BestAppEver
+//
+//  Created by Chris on 2015-05-06.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
