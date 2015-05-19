1068e70824cf125a9e55ec1189e3477745b9b6e9
Buddy Build <buddybuild@mail.ernestgroup.com>
2015-05-18 22:01:25 -0700

    Adding BuddyBuildSDK

--- BestAppEver/AppDelegate.m
+++ BestAppEver/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <BuddyBuildSDK/BuddyBuildSDK.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [BuddyBuildSDK setup:self];
+    
     // Override point for customization after application launch.
     return YES;
 }
@@ -40,6 +43,16 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    if ([BuddyBuildSDK handleOpenURL:url]) {
+        return YES;
+    }
+    return NO;
 }
 
 @end
